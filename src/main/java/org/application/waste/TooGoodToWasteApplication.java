34843deb8f1b6a56c4a87cec5dc75d06dc22801b
Constantin_Stamate <constantinstamate.r@gmail.com>
2025-09-02 11:44:11 +0300

    create first commit

---
+++ src/main/java/org/application/waste/TooGoodToWasteApplication.java
@@ -0,0 +1,12 @@
+package org.application.waste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TooGoodToWasteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TooGoodToWasteApplication.class, args);
+    }
+}
---
+++ src/test/java/org/application/waste/TooGoodToWasteApplicationTests.java
@@ -0,0 +1,12 @@
+package org.application.waste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TooGoodToWasteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
