6cdef289b43a1393796cd19a5dee412ef9a0ec11
Andriana Golban <andriana.golbana@isa.utm.md>
2025-09-16 13:18:12 +0300

    Security configuration

--- src/main/java/org/application/waste/config/SecurityConfig.java
+++ src/main/java/org/application/waste/config/SecurityConfig.java
@@ -6,14 +6,15 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig {
+public class
+SecurityConfig {
+
     private final UserDetailsService userDetailsService;
 
     public SecurityConfig(UserDetailsService userDetailsService) {
@@ -26,22 +27,24 @@ public class SecurityConfig {
     }
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain filterChain(HttpSecurity http,
+                                           CustomLoginFailureHandler failureHandler,
+                                           CustomLoginSuccessHandler successHandler) throws Exception {
         http.csrf().disable()
                 .authorizeHttpRequests((authorize) ->
                         authorize
                                 .requestMatchers("/register/**").not().authenticated()
                                 .requestMatchers(
                                         "/css/**", "/js/**", "/images/**", "/lib/**", "/scss/**",
-                                        "/index", "/login"
+                                        "/index", "/login/**"
                                 ).permitAll()
                                 .anyRequest().authenticated()
                 )
                 .formLogin(
                         form -> form
                                 .loginPage("/login")
-                                .defaultSuccessUrl("/index", true)
-                                .failureUrl("/login?error")
+                                .successHandler(successHandler)
+                                .failureHandler(failureHandler)
                                 .permitAll()
                 )
                 .logout(
@@ -51,10 +54,8 @@ public class SecurityConfig {
                                 .deleteCookies("JSESSIONID")
                                 .logoutSuccessUrl("/login?logout=true")
                                 .permitAll()
-                )
-                .sessionManagement(session -> session
-                        .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
                 );
+
         return http.build();
     }
 
