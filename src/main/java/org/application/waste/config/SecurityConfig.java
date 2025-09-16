dd4e669ba970bbf6e1de7ed290737d83b148ed09
Gutan Marina <gutanmarina123@gmail.com>
2025-09-16 14:59:45 +0300

    updated security configurations for functionality

--- src/main/java/org/application/waste/config/SecurityConfig.java
+++ src/main/java/org/application/waste/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package org.application.waste.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -34,9 +35,12 @@ SecurityConfig {
                 .authorizeHttpRequests((authorize) ->
                         authorize
                                 .requestMatchers("/register/**").not().authenticated()
+                                .requestMatchers("/forgotPassword/**").permitAll()
+                                .requestMatchers(HttpMethod.GET, "/reset-password/**").permitAll()
+                                .requestMatchers(HttpMethod.POST, "/reset-password/**").permitAll()
                                 .requestMatchers(
                                         "/css/**", "/js/**", "/images/**", "/lib/**", "/scss/**",
-                                        "/index", "/login/**"
+                                        "/index", "/login/**","/forgotPassword/**", "/reset-password/**"
                                 ).permitAll()
                                 .anyRequest().authenticated()
                 )
