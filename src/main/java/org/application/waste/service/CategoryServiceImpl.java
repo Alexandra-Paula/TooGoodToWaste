d73cb8819205a5b8ad8f016670c96c2a1d3f23ed
Constantin_Stamate <constantinstamate.r@gmail.com>
2025-09-22 11:30:07 +0300

    update category service with new functionality

--- src/main/java/org/application/waste/service/CategoryServiceImpl.java
+++ src/main/java/org/application/waste/service/CategoryServiceImpl.java
@@ -2,13 +2,20 @@ package org.application.waste.service;
 
 import org.application.waste.entity.Category;
 import org.application.waste.repository.CategoryRepository;
+import org.application.waste.repository.ProductRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Service
 public class CategoryServiceImpl implements CategoryService {
     private final CategoryRepository categoryRepository;
+    private final ProductRepository productRepository;
 
-    public CategoryServiceImpl(CategoryRepository categoryRepository) {
+    public CategoryServiceImpl(CategoryRepository categoryRepository, ProductRepository productRepository) {
+        this.productRepository = productRepository;
         this.categoryRepository = categoryRepository;
     }
 
@@ -20,5 +27,25 @@ public class CategoryServiceImpl implements CategoryService {
     @Override
     public Category findByName(String name) {
         return categoryRepository.findByCategoryName(name).orElse(null);
+    }
+
+    @Override
+    public Map<Category, Integer> getProductCountPerCategory() {
+        List<Object[]> results = productRepository.countProductsPerCategory();
+        Map<Category, Integer> categoryCountMap = new HashMap<>();
+
+        for (Object[] row : results) {
+            Long categoryId = (Long) row[0];
+            String categoryName = (String) row[1];
+            Long count = (Long) row[2];
+
+            Category category = new Category();
+            category.setId(categoryId);
+            category.setCategoryName(categoryName);
+
+            categoryCountMap.put(category, count.intValue());
+        }
+
+        return categoryCountMap;
     }
 }
