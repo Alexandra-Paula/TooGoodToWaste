149aa6b682235dcc3639fc487e5c0d07d7f2b03d
Constantin_Stamate <constantinstamate.r@gmail.com>
2025-09-25 13:46:43 +0300

    add checkout repository for managing orders

---
+++ src/main/java/org/application/waste/repository/CheckoutRepository.java
@@ -0,0 +1,9 @@
+package org.application.waste.repository;
+
+import org.application.waste.entity.Checkout;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CheckoutRepository extends JpaRepository<Checkout, Long> {
+}
